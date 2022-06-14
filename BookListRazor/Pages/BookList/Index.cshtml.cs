c414c201f15c9fc24b294ad8bc462b254b185a70
hsupanunt <hataipat.supanunt@agoda.com>
2022-06-14 22:58:05 +0700

    add GET handler

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -1,10 +1,18 @@
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookListRazor.Pages.BookList
-{
+
+{ 
     public class IndexModel : PageModel
     {
+
+    private readonly ApplicationDBContext _db;
+
+        public IndexModel(ApplicationDBContext db) {_db = db; } 
+       
+        public IEnumerable<Book> Books { get; set; }
         public void OnGet()
         {
         }
