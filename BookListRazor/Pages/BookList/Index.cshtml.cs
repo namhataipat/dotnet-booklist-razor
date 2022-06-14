0e69f757cec047e055596bcdb889272e23d468a2
hsupanunt <hataipat.supanunt@agoda.com>
2022-06-14 23:30:51 +0700

    finish UI

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -1,6 +1,8 @@
 using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace BookListRazor.Pages.BookList
 
@@ -12,9 +14,10 @@ namespace BookListRazor.Pages.BookList
 
         public IndexModel(ApplicationDBContext db) {_db = db; } 
        
-        public IEnumerable<Book> Books { get; set; }
-        public void OnGet()
+        public System.Collections.Generic.IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
         {
+            Books = await _db.Book.ToListAsync();
         }
     }
 }
